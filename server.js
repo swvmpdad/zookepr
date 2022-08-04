817fabb0b4a50fdf952e9de68ac5234e1dcc7410
swvmpdad <will.g.camp@gmail.com>
2022-08-03 21:16:45 -0500

    added front end functionality

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 const { animals } = require('./data/animals');
 
 
@@ -104,7 +105,19 @@ app.post('/api/animals', (req, res) => {
   });
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './pubilc/index.html'));
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
   app.listen(PORT, () => {
